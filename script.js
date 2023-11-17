39a82fc5dc47cb125ab6f0ff99b2b0671e687c1a
yungshun317 <seraph317@gmail.com>
2023-11-17 08:45:51 +0800

    [FEAT] Query elements

---
+++ script.js
@@ -0,0 +1,16 @@
+const inputWeight = document.querySelector('.gold-price-calculator__weight-input');
+const inputWeightFromUnit = document.querySelector('.gold-price-calculator__weight-from-unit-select');
+const inputWeightToUnit = document.querySelector('.gold-price-calculator__weight-to-unit-select');
+const inputPercentage = document.querySelector('.gold-price-calculator__percentage-input');
+const inputGoldPrice = document.querySelector('.gold-price-calculator__gold-price-input');
+const inputFee = document.querySelector('.gold-price-calculator__fee-input');
+const inputToCurrency = document.querySelector('.gold-price-calculator__to-currency-select');
+const inputExchangeRate = document.querySelector('.gold-price-calculator__exchange-rate-input');
+
+const outputToWeightText = document.querySelector('.gold-price-calculator__to-weight-text');
+const outputRealWeightText = document.querySelector('.gold-price-calculator__real-weight-text');
+const outputAmountNumberText = document.querySelector('.gold-price-calculator__amount-number-text');
+const outputAmountWordText = document.querySelector('.gold-price-calculator__amount-word-text');
+const convertedAmountText = document.querySelector('.gold-price-calculator__converted-amount-text');
+const convertedAmountPerWeightText = document.querySelector('.gold-price-calculator__converted-amount-per-weight-text');
+
